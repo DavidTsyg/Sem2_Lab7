112273d746d69edb09a4396d29399738692931c0
DavidTsyg <davidtsyg@gmail.com>
2017-05-19 00:21:26 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -4,11 +4,11 @@
 
 SCENARIO ("ConstructorTest(FileExists)"){
 	Matrix<int> m;
-	rv = m.fill_by_file("test.txt");
+	int rv = m.fill_by_file("test.txt");
 	REQUIRE (rv == 1);
 }
 SCENARIO ("ConstructorTest(FileDoesn'tExist)"){
 	Matrix<int> m;
-	rv = m.fill_by_file("test.txt");
+	int rv = m.fill_by_file("Doesn'tExist.txt");
 	REQUIRE (rv == 0);
 }
